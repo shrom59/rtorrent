0620c9fde2f5ac26459857d1ceedf6b79de55393
Romain C <couillet.romain@yahoo.fr>
2016-03-20 11:17:57 +0100

    Add details on status.sh

--- include/dostatus.sh
+++ include/dostatus.sh
@@ -13,8 +13,20 @@ do_status() {
    echo -e "- Status                :""\\033[1;31m"" Failed ""\\033[0;39m"
   fi
   echo "- Base Path             : ${BASEPATH}"
-  echo "- Download Path         : $destfolder"
   echo "- Config File           : ${CONFIGPATH}"
+  echo "- Download Path         : $(cat $CONFIGPATH |grep "directory =" |awk '{print $3}')"
+  if [ "fping $mountpointaddress" ];
+  then 
+	  echo -e "- Mounting point status : ""\\033[1;32m""Alive""\\033[0;39m"
+  else
+	  echo -e "- Mounting point status : ""\\033[1;31m""Unreacheable""\\033[0;39m"
+  fi 
+  if [ "$mounttest" ];
+	then
+	  echo "- Mount point           : $destfolder is mounted to $mountpoint"
+  else
+          echo "- Mount point           : $destfolder seems not mounted !"
+  fi
   echo "- Tmux Session Name     : ${SCRNAME}"
   echo "- rTorrent Session Path : ${SESSIONPATH}"
   echo "- Process ID            : "${PID}""
